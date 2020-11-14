1d67abc78322f99c770254084e19451c28d67889
Hadrien Breton <2056621@cstj.qc.ca>
2020-11-14 13:11:13 -0500

    Créé le projet et les fonctions des menus.

--- Formes2056621/Fonctionss2056621.h
+++ Formes2056621/Fonctionss2056621.h
@@ -1,1 +1,30 @@
 #pragma once
+
+
+void afficherMenu1();
+
+void afficherMenu2();
+
+validerMenu
+
+traiterRectangle
+
+traiterCarre
+
+traiterTriangle
+
+traiterLosange
+
+dessinerRectangle
+
+dessinerTriangle1
+
+dessinerTriangle2
+
+dessinerTriangle3
+
+dessinerTriangle4
+
+dessinerLosange
+
+genererNombreAleatoire
